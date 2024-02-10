2ce19ceb043245f3325ae457c60c7f66fba31f3d
xHurricane <molham.work9@gmail.com>
2024-02-10 21:02:41 +0200

    Hid Token

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const Telegram = require('grammy');
 const TOKEN = process.env.TOKEN;
 
-const bot = new Telegram.Bot("6989861872:AAGS3k5BTWo1SK7SURCB2JpX79XUQ4uw4gs");
+const bot = new Telegram.Bot(TOKEN);
 
 bot.on("message", ctx => {
     ctx.reply("Hi everyone!");
