d3661c66d467ed0390b15890b6bc3bf73d3816e5
xHurricane <molham.work9@gmail.com>
2024-02-10 21:37:38 +0200

    Added it

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 require('dotenv').config();
+const express = require('express');
+const app = express();
+app.use(express.json());
 const Telegram = require('grammy');
 const TOKEN = process.env.TOKEN;
-
 const bot = new Telegram.Bot(TOKEN);
 
 bot.on("message", ctx => {
+    console.log("RECIEVED MESSAGE");
     ctx.reply("Hi everyone!");
-})
+});
 
-bot.start();
+app.post('/webhook', Telegram.webhookCallback(bot, "express"));
+
+app.listen(3000, () => {
+    bot.api.setWebhook("" + process.env.SERVER_URL + "/webhook");
+    console.log("Running!");
+})
